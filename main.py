da20b91558ca31a022981ed7317e5c1c4dc242cd
RACollins <richardcollins128@mail.com>
2024-10-23 23:46:21 +0900

    added replaced pico with tailwind

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 from fasthtml.common import *
 
-app, rt = fast_app()
+tailwind = Script(src="https://cdn.tailwindcss.com")
+app, rt = fast_app(live=True, pico=False, hdrs=[tailwind])
+
+
+def with_card(colour: str, border_colour: str, padding: str):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            content = func(*args, **kwargs)
+            tailwind_classes = f"p-{padding} rounded-lg shadow-lg bg-{colour}-100 border border-{border_colour}-500"
+            return Div(Div(content, cls=tailwind_classes), cls=f"card border border-{border_colour}-500 shadow-lg rounded-lg m-4")
+
+        return wrapper
+
+    return decorator
+
+
+@with_card(colour="lightblue", border_colour="blue", padding="10")
+def Card():
+    return Div(H1("Title"), P("Description"), P("Another decsription"))
 
 
 @rt("/")
 def get():
-    return Div(
-        P("Hello World!"),
-        A("Click Me!", hx_get="/change", hx_target="div")
+    return Titled(
+        Div(P("Hello World!"), A("Click Me!", hx_get="/change", hx_target="div"))
     )
 
 
 @rt("/change")
 def get():
-    return Div(
-        P("Nice to be here!"),
-        A("Go Back!", hx_get="/", hx_target="div")
-    )
+    return Div(P("Nice to be here!"), A("Go Back!", hx_get="/", hx_target="div"), Card())
 
 
 serve()
