56afbc110277e1bf3f72d3cc187b07b8328df674
RACollins <richardcollins128@mail.com>
2024-10-28 23:53:11 +0900

    got tailwindcss intellisence to work

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from pages.landing import Page
 tailwind = Script(src="https://cdn.tailwindcss.com")
 app, rt = fast_app(live=True, pico=False, hdrs=[tailwind])
 
+
 @rt("/")
 def get():
     return Page()
+
 
 serve()
